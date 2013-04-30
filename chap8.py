cb752b2f6588a42865bab768fe1be290b127bc45
Brian Yee <yee.brianj@gmail.com>
2013-04-29 22:42:47 -0400

    rot13

--- chap8.py
+++ chap8.py
@@ -1,2 +1,16 @@
 # This is where the answers to Chapter 8 questions for the BSS Dev RampUp go
-# Name:
+# Name: Brian Yee
+
+def rotate_word(s, i):
+    upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lower = 'abcdefghijklmnopqrstuvwxyz'
+    rotated = ''
+    for letter in s:
+        for character in range(len(upper)):
+            if letter == upper[character]:
+                rotated += upper[(character+i)%26]
+        for character in range(len(lower)):
+            if letter == lower[character]:
+                rotated += lower[(character+i)%26]
+    return rotated
+    
